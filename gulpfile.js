3745926e342d3b7de2ea749a324f4cf2c6cb9065
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-06-23 13:04:00 -0700

    added jshint-stylish

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 'use strict';
 
-module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-jshint')
-}
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function() {
+  return gulp.src('gulpfile.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+
+gulp.task('default', ['lint']);
