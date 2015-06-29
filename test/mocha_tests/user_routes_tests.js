b29cbefdef010f75eed6346abbf10b80ed1e1f35
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-06-29 15:29:53 -0700

    created the beginning of tests, put standards in table form instead of list form, and added standard description to form

---
+++ test/mocha_tests/user_routes_tests.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var chai = require('chai');
+var chaihttp = require('chai-http');
+var expect = chai.expect;
+var app = 'localhost:3000';
+chai.use(chaihttp);
+
+var User = require('../model/User');
+
+describe('The login API', function() {
+  describe('Creating a user', function() {
+
+  });
+});
