a3f8ffe526fef2d4f60b998dd134b47316b02738
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-06-30 15:17:03 -0700

    created routes for test

--- app/js/standards/controllers/standards_controller.js
+++ app/js/standards/controllers/standards_controller.js
@@ -4,21 +4,6 @@ module.exports = function(app) {
   app.controller('standardsController', ['$scope', 'RESTResource', 'copy', function($scope, resource, copy) {
 
     /**
-     * Tells whether the form should be shown or not, false means the list of standards should be shown, true means form to add a standard is shown
-     * @type {Boolean}
-     */
-    $scope.formShowing = false;
-
-    /**
-     * Holds the standard that has been clicked.
-     */
-    $scope.displayedStandard;
-
-    $scope.setDisplayedStandard = function(standard) {
-      $scope.displayedStandard = standard;
-    };
-
-    /**
      * Calls on our REST resource to hit the API at /api/standards
      * @type {[type]}
      */
@@ -37,31 +22,62 @@ module.exports = function(app) {
     $scope.errors = [];
 
     $scope.quantity = 5;
+
+    /**
+     * Tells whether the form should be shown or not, false means the list of standards should be shown, true means form to add a standard is shown
+     * @type {Boolean}
+     */
+    $scope.formShowing = false;
+
+    $scope.toggleEdit = function(standard) {
+      if ($scope.formShowing) {
+        $scope.formShowing = false;
+        return;
+      } else {
+        if (standard) {
+          standard.editing = true;
+          $scope.formShowing = true;
+          $scope.standard = standard;
+          return;
+        } else {
+          $scope.standard = null;
+          $scope.formShowing = true;
+          return;
+        }
+      }
+    };
+
+    /**
+     * Holds the standard that has been clicked.
+     */
+    $scope.displayedStandard = [];
+
+    $scope.setDisplayedStandard = function(standard) {
+      $scope.displayedStandard.splice(0, 1, standard);
+    };
+
     /**
      * Will make a GET request to /api/standards and return an array of standards to be displayed
      */
     $scope.getAll = function() {
       Standard.getAll(function(err, data) {
         if (err) {
-          return $scope.errors.push({'msg': 'Error retrieving food items'});
+          return $scope.errors.push({
+            'msg': 'Error retrieving standards'
+          });
         }
         $scope.standards = data;
       });
     };
 
-
-    $scope.showForm = function() {
-      if($scope.formShowing) {
-        $scope.formShowing = false;
-        return;
-      }
-      $scope.formShowing = true;
-    };
-
+    /**
+     * Will add standard to display, hide the form for adding a standard, and make a POST request to the API to create a new standard
+     * @param  {object} standard The specified standard to create
+     */
     $scope.createNewStandard = function(standard) {
       var newStandard = copy(standard);
       $scope.standards.push(newStandard);
-
+      $scope.displayedStandard.splice(0, 1, newStandard);
       Standard.create(newStandard, function(err, data) {
         if (err) {
           return $scope.errors.push({
@@ -69,7 +85,38 @@ module.exports = function(app) {
           });
         }
         $scope.standards.splice($scope.standards.indexOf(newStandard), 1, data);
+        $scope.displayedStandard.splice(0, 1, data);
         $scope.formShowing = false;
+      });
+    };
+
+    /**
+     * Will update a standard on the client and make a PUT request to the server to update the standard
+     * @param  {object} standard The specified standard to update
+     */
+    $scope.saveStandard = function(standard) {
+      Standard.save(standard, function(err, data) {
+        if (err) return $scope.errors.push({
+          'msg': 'There was an error while updating this standard'
+        });
+
+        $scope.standard = null;
+        $scope.formShowing = false;
+        standard.editing = false;
+      });
+    };
+
+    /**
+     * Will remove standard from display and make delete request to API to remove from database
+     * @param  {object} standard The specified standard to delete
+     */
+    $scope.removeStandard = function(standard) {
+      $scope.standards.splice($scope.standards.indexOf(standard), 1);
+      $scope.displayedStandard = [];
+      Standard.remove(standard, function(err) {
+        if(err) return $scope.errors.push({
+          'msg': 'There was an error deleting this standard'
+        });
       });
     };
   }]);
