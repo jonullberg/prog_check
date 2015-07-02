cee1284d45ed7c64657aa237261d4c6a833aa5d7
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-07-02 08:33:53 -0700

    added question form and test controller

---
+++ app/js/tests/controllers/test_controller.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function(app) {
+  app.controller('testController', ['$scope', 'RESTResource', 'dataStore', function($scope, resource, dataStore) {
+
+    var Test = resource('tests');
+
+    $scope.tests = [];
+
+    $scope.errors = [];
+
+    $scope.getAll = function() {
+      Test.getAll(function(err, data) {
+        if (err) {
+          return $scope.errors.push({
+            'msg': 'Error retrieving tests'
+          });
+        }
+
+        dataStore.tests = data;
+        $scope.tests = data;
+
+      });
+    };
+
+    $scope.addQuestion = function() {
+
+    };
+
+    $scope.saveQuestion = function(question) {
+
+    };
+  }]);
+};
