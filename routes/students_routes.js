487ffa1f52d7c749a9ec24e5f39597c8527922dc
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-10-10 12:48:11 -0700

    created POST route to update students goals

--- routes/students_routes.js
+++ routes/students_routes.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var Students = require('../models/Student');
+var Attempts = require('../models/Attempt');
+var Standards = require('../models/Standard');
 var bodyparser = require('body-parser');
 var eatAuth = require('../lib/eat_auth')(process.env.APP_SECRET);
 
@@ -120,4 +122,70 @@ module.exports = function(router, passport) {
 
     });
   });
+  router.get('/students/:studentId/attempts/', eatAuth, function(req, res) {
+    Attempts.find({'studentId': req.params.studentId}, function(err, data) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          'msg': 'Internal Server Error'
+        });
+      }
+      res.json({
+        'data': data
+      });
+    });
+  });
+  router.put('/students/:studentId/attempts/:attemptId', eatAuth, function(req, res) {
+    Attempts.find({
+      'studentId': req.params.studentId,
+      '_id': req.params.attemptId
+    }, function(err, data) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          'msg': 'Internal Server Error'
+        });
+      }
+      res.json({
+        'data': data
+      });
+    });
+  });
+  /**
+   * A POST route to add goals to a specific student. This route had to be authenticated with an EAT token
+   * @param  {Object} req  The request coming from the client
+   * @param  {Object} res) The response object
+   */
+  router.post('/students/:studentId/goals/', eatAuth, function(req, res) {
+    var goal = req.body;
+    Standards.find().elemMatch('goals', { _id: { $in: [goal.goalId]}} ).exec(function(err, data) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          'msg': 'Internal Server Error'
+        });
+      }
+      var returnedGoal = data[0].goals.id(goal.goalId);
+      if (returnedGoal.name) {
+        goal.description = returnedGoal.name;
+      } else if (returnedGoal.description) {
+        goal.description = returnedGoal.description;
+      } else {
+        goal.description = null;
+      }
+      goal.active = true;
+      Students.update({'_id': req.params.studentId}, { $push: {'goals': goal}}, function(err, data) {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            'msg': 'Internal Server Error'
+          });
+        }
+        res.json({
+          'msg': 'Success'
+        });
+      });
+    });
+  });
+
 };
