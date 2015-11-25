bae5538f59a69ff2aa68d60fcd2163b70701054f
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-11-25 07:59:03 -0800

    removed console.log

--- app/js/teachers/data_services/teacher_attempts_data.js
+++ app/js/teachers/data_services/teacher_attempts_data.js
@@ -54,7 +54,6 @@ module.exports = function(app) {
     function fetchAttemptsByGoal(studentId, goalId, cb) {
       $http.get('/api/students/' + studentId + '/attempts?goalId=' + goalId)
         .then(function(response) {
-          console.log(response);
           this.setAttempts(response.data.attempts);
           this.setResults(response.data.results);
           handleCallback(cb, response);
