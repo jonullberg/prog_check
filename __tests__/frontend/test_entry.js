adceeb6875cc5f023f8af098cdbc581484f06b10
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2016-03-13 11:07:31 -0700

    Add testing with Karma and Webpack

--- __tests__/frontend/test_entry.js
+++ __tests__/frontend/test_entry.js
@@ -1,3 +1,3 @@
-require('./controllers/goal_form_controller-test.js');
-require('./controllers/admin_teachers_list_controller-test.js');
+require('./admin/standards-list/admin.standards-list.controller.spec.js');
+// require('./controllers/admin_teachers_list_controller-test.js');
 // require('./controllers/tests_controller_tests.js');
